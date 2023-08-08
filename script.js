692d7bef4b0fa14d0d46f02760095c74b64f5ec1
Emmanuel Guillen <emmanuelguillen@Emmanuels-Mac-mini.local>
2023-08-07 21:42:07 -0700

    HTML, CSS, and JS doc updated. Website nav more mobile friendly

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function hamburgerMenu() {
+    var a = document.getElementById("hamburger-menu");
+    var b = document.getElementById("close-menu");
     var x = document.getElementById("nav-options");
-    if (x.style.display == "block") {
+    var y = document.getElementById("nav-button");
+    if ((x.style.display == "block") && (y.style.display == "block")) {
         x.style.display = "none";
+        y.style.display = "none";
+        a.style.display = "block";
+        b.style.display = "none";
     } else {
         x.style.display = "block";
+        y.style.display = "block";
+        a.style.display = "none";
+        b.style.display = "block";
         }
-    }
+}
